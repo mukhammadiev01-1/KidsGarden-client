6715817a9fe9cb20df688bc35d61e2aae78cdab4
mukhammadiev01-1 <shmukhammadiev01@gmail.com>
2026-07-03 05:23:41 +0900

    fix: include public assets and production font loading

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from 'next/app';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { CssBaseline } from '@mui/material';
 import React, { useState } from 'react';
+import { Poppins } from 'next/font/google';
 import { light } from '../scss/MaterialTheme';
 import { ApolloProvider } from '@apollo/client';
 import { useApollo } from '../apollo/client';
@@ -12,6 +13,13 @@ import '../scss/app.scss';
 import '../scss/pc/main.scss';
 import '../scss/mobile/main.scss';
 
+const poppins = Poppins({
+	subsets: ['latin'],
+	weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+	display: 'swap',
+	variable: '--font-poppins',
+});
+
 const App = ({ Component, pageProps }: AppProps) => {
 	// @ts-ignore
 	const [theme, setTheme] = useState(createTheme(light));
@@ -19,13 +27,15 @@ const App = ({ Component, pageProps }: AppProps) => {
 	const googleClientId = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID;
 
 	const appContent = (
-		<ApolloProvider client={client}>
-			<ThemeProvider theme={theme}>
-				<CssBaseline />
-				<Component {...pageProps} />
-				<AiAssistant />
-			</ThemeProvider>
-		</ApolloProvider>
+		<div className={poppins.variable}>
+			<ApolloProvider client={client}>
+				<ThemeProvider theme={theme}>
+					<CssBaseline />
+					<Component {...pageProps} />
+					<AiAssistant />
+				</ThemeProvider>
+			</ApolloProvider>
+		</div>
 	);
 
 	return googleClientId ? (
